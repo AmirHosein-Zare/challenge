4a9bb4e5e321613d4d98987471d33a105d55f231
AmirHosein Zare <awir.software@gmail.com>
2023-07-24 13:09:59 +0330

    Update 1.2.js

--- 1.2.js
+++ 1.2.js
@@ -1,6 +1,6 @@
 // getting the data from api
 const fetchPromise = async () => {
-  await fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json')
+  return await fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json')
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(err => console.log(err))
